b8654b269d0f6d94d35c196fc993f5112ba5922f
Kyrylo Polezhaiev <polezhaiev@ukr.net>
2016-03-23 16:17:23 +0200

    win32: Add Makefile for Windows cross-compilation

---
+++ i686-w64-mingw32.mk
@@ -0,0 +1,139 @@
+AR=i686-w64-mingw32-ar
+CC=i686-w64-mingw32-gcc
+CXX=i686-w64-mingw32-g++
+RANLIB=i686-w64-mingw32-ranlib
+TARGET_DIR=./build/
+
+CXXFLAGS=--std=gnu++17 -fpermissive
+
+CFLAGS= \
+-DKURENTO_MODULES_DIR="\"./modules/\"" \
+-DPACKAGE="\"kms-filters\"" \
+-DVERSION="\"6.4.1.6\"" \
+-Duint="unsigned" \
+-DWIN32_LEAN_AND_MEAN=1 \
+-I./win32/server/implementation/HttpServer/ \
+-I/usr/i686-w64-mingw32/sys-root/mingw/include/nice \
+-I/usr/i686-w64-mingw32/sys-root/mingw/include/libsoup-2.4 \
+-I/usr/i686-w64-mingw32/sys-root/mingw/include/gstreamer-1.0 \
+-I/usr/i686-w64-mingw32/sys-root/mingw/lib/gstreamer-1.0/include \
+-I/usr/i686-w64-mingw32/sys-root/mingw/include/glib-2.0 \
+-I/usr/i686-w64-mingw32/sys-root/mingw/lib/glib-2.0/include \
+-I/usr/i686-w64-mingw32/sys-root/mingw/include/glibmm-2.4 \
+-I/usr/i686-w64-mingw32/sys-root/mingw/lib/glibmm-2.4/include \
+-I/usr/i686-w64-mingw32/sys-root/mingw/include/sigc++-2.0 \
+-I/usr/i686-w64-mingw32/sys-root/mingw/lib/sigc++-2.0/include \
+-I../kms-core/win32/server/implementation/generated-cpp/ \
+-I../kms-core/win32/server/interface/generated-cpp/ \
+-I../kms-core/src/server/interface/ \
+-I../kms-core/src/server/implementation/ \
+-I../kms-core/src/server/implementation/objects/ \
+-I../kms-core/src/gst-plugins/ \
+-I./win32/server/implementation/generated-cpp/ \
+-I./win32/server/interface/generated-cpp/  \
+-I./win32/gst-plugins/ \
+-I./win32 \
+-I./src/server/implementation/ \
+-I./src/server/implementation/objects/ \
+-I./src/server/interface/ \
+-I./src/gst-plugins/ \
+-I./src/gst-plugins/commons/ \
+-I../jsoncpp/include/ \
+-I../kms-jsonrpc/src/
+
+LIBS= \
+-L/usr/i686-w64-mingw32/sys-root/mingw/lib \
+-L/usr/lib/gcc/i686-w64-mingw32/5.2.0 \
+-L/usr/i686-w64-mingw32/lib/ \
+-L../kms-jsonrpc/build/ \
+-L../jsoncpp/build/ \
+-L../kms-core/build/ \
+-L./build/
+
+FACEOVERLAY_OBJS=$(FACEOVERLAY_SRC:.c=.o)
+LOGOOVERLAY_OBJS=$(LOGOOVERLAY_SRC:.cpp=.o)
+IMAGEOVERLAY_OBJS=$(IMAGEOVERLAY_SRC:.cpp=.o)
+FACEDETECTOR_OBJS=$(FACEDETECTOR_SRC:.c=.o)
+MOVEMENTDETECTOR_OBJS=$(MOVEMENTDETECTOR_SRC:.cpp=.o)
+OPENCVFILTER_OBJS=$(OPENCVFILTER_SRC:.c=.o)
+KMSFILTERSINTERFACE_OBJS=$(KMSFILTERSINTERFACE_SRC:.c=.o)
+KMSFILTERSIMPL_OBJS=$(KMSFILTERSIMPL_SRC:.cpp=.o)
+KMSFILTERSMODULE_OBJS=$(KMSFILTERSMODULE_SRC:.c=.o)
+
+all: \
+$(TARGET_DIR)/$(FACEOVERLAY_TARGET) \
+$(TARGET_DIR)/$(LOGOOVERLAY_TARGET) \
+$(TARGET_DIR)/$(IMAGEOVERLAY_TARGET) \
+$(TARGET_DIR)/$(FACEDETECTOR_TARGET) \
+$(TARGET_DIR)/$(MOVEMENTDETECTOR_TARGET) \
+$(TARGET_DIR)/$(OPENCVFILTER_TARGET) \
+\
+$(TARGET_DIR)/$(KMSFILTERSINTERFACE_TARGET) \
+$(TARGET_DIR)/$(KMSFILTERSIMPL_TARGET) \
+$(TARGET_DIR)/$(KMSFILTERSMODULE_TARGET)
+
+$(TARGET_DIR)/$(KMSFILTERSINTERFACE_TARGET): $(KMSFILTERSINTERFACE_OBJS)
+	mkdir -p $(TARGET_DIR)
+	$(AR) cr $@ $^
+	$(RANLIB) $@
+
+$(TARGET_DIR)/$(KMSFILTERSIMPL_TARGET): $(KMSFILTERSIMPL_OBJS)
+	mkdir -p $(TARGET_DIR)
+	$(CXX) -shared -o $@ $(CFLAGS) $^ $(KMSFILTERSIMPL_LIBS) -Wl,--out-implib,$@.a
+
+$(TARGET_DIR)/$(KMSFILTERSMODULE_TARGET): $(KMSFILTERSMODULE_OBJS)
+	mkdir -p $(TARGET_DIR)
+	$(CXX) -shared -o $@ $(CFLAGS) $^ $(KMSFILTERSMODULE_LIBS) -Wl,--out-implib,$@.a
+
+$(TARGET_DIR)/$(FACEOVERLAY_TARGET): $(FACEOVERLAY_OBJS)
+	mkdir -p $(TARGET_DIR)
+	$(CXX) -shared -o $@ $(CFLAGS) $^ $(FACEOVERLAY_LIBS)
+
+$(TARGET_DIR)/$(LOGOOVERLAY_TARGET): $(LOGOOVERLAY_OBJS)
+	mkdir -p $(TARGET_DIR)
+	$(CXX) -shared -o $@ $(CFLAGS) $^ $(LOGOOVERLAY_LIBS)
+
+$(TARGET_DIR)/$(IMAGEOVERLAY_TARGET): $(IMAGEOVERLAY_OBJS)
+	mkdir -p $(TARGET_DIR)
+	$(CXX) -shared -o $@ $(CFLAGS) $^ $(IMAGEOVERLAY_LIBS)
+
+$(TARGET_DIR)/$(FACEDETECTOR_TARGET): $(FACEDETECTOR_OBJS)
+	mkdir -p $(TARGET_DIR)
+	$(CXX) -shared -o $@ $(CFLAGS) $^ $(FACEDETECTOR_LIBS)
+
+$(TARGET_DIR)/$(MOVEMENTDETECTOR_TARGET): $(MOVEMENTDETECTOR_OBJS)
+	mkdir -p $(TARGET_DIR)
+	$(CXX) -shared -o $@ $(CFLAGS) $^ $(MOVEMENTDETECTORPLIBS)
+
+$(TARGET_DIR)/$(OPENCVFILTER_TARGET): $(OPENCVFILTER_OBJS)
+	mkdir -p $(TARGET_DIR)
+	$(CXX) -shared -o $@ $(CFLAGS) $^ $(OPENCVFILTER_LIBS)
+
+%.o: %.c
+	$(CC) -c $(CFLAGS) -o $@ $<
+
+%.o: %.cpp
+	$(CXX) -c $(CFLAGS) $(CXXFLAGS) -o $@ $<
+
+.PHONY: clean
+clean:
+	rm -f $(TARGET_DIR)/$(FACEOVERLAY_TARGET)
+	rm -f $(FACEOVERLAY_OBJS)
+	rm -f $(TARGET_DIR)/$(LOGOOVERLAY_TARGET)
+	rm -f $(LOGOOVERLAY_OBJS)
+	rm -f $(TARGET_DIR)/$(IMAGEOVERLAY_TARGET)
+	rm -f $(IMAGEOVERLAY_OBJS)q
+	rm -f $(TARGET_DIR)/$(FACEDETECTOR_TARGET)
+	rm -f $(FACEDETECTOR_OBJS)
+	rm -f $(TARGET_DIR)/$(MOVEMENTDETECTOR_TARGET)
+	rm -f $(MOVEMENTDETECTOR_OBJS)
+	rm -f $(TARGET_DIR)/$(OPENCVFILTER_TARGET)
+	rm -f $(OPENCVFILTER_OBJS)
+	rm -f $(TARGET_DIR)/$(KMSFILTERSINTERFACE_TARGET)
+	rm -f $(KMSFILTERSINTERFACE_OBJS)
+	rm -f $(TARGET_DIR)/$(KMSFILTERSIMPL_TARGET)
+	rm -f $(TARGET_DIR)/$(KMSFILTERSIMPL_TARGET).a
+	rm -f $(KMSFILTERSIMPL_OBJS)
+	rm -f $(TARGET_DIR)/$(KMSFILTERSMODULE_TARGET)
+	rm -f $(KMSFILTERSMODULE_OBJS)
+
